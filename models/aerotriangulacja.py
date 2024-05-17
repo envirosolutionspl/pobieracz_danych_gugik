46ba79c269779b4d038e2a79087e828fdfd164cb
Wojciech Sołyga <113895660+wojtek19022@users.noreply.github.com>
2024-05-17 15:52:59 +0200

    Squashed commit of the following:
    commit a12f424918436052c166cc60b0d9a68ca530af77
    Merge: e486dcc 8538415
    Author: Wojciech Sołyga <113895660+wojtek19022@users.noreply.github.com>
    Date:   Sat Apr 13 11:09:57 2024 +0200
    
        Merge pull request #79 from envirosolutionspl/master
    
        v1.0.10
    
    commit 853841514d54b2cf5b256ded7b16b894b12b10fb
    Author: envirosolutionspl <office@envirosolutions.pl>
    Date:   Thu Jan 25 07:04:39 2024 +0100
    
        v1.0.10

--- models/aerotriangulacja.py
+++ models/aerotriangulacja.py
@@ -1,14 +1,18 @@
 import datetime
+
+
 class Aerotriangulacja:
     def __init__(
             self,
             url,
             id,
-            zgloszenie
+            zgloszenie,
+            dt_pzgik
     ):
         self.url = url
         self.id = id
         self.zgloszenie = zgloszenie
+        self.dtPzgik = str(dt_pzgik)
 
     def __eq__(self, other):
         return self.url == other.url
