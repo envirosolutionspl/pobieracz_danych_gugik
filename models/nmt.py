0416293532d82d601a46047a64a4755edb63fa02
wojtek19022 <113895660+wojtek19022@users.noreply.github.com>
2024-06-08 15:28:26 +0200

    Naprawa pobierania NMT

--- models/nmt.py
+++ models/nmt.py
@@ -10,6 +10,7 @@ class Nmt:
             format,
             charakterystykaPrzestrzenna,
             bladSredniWysokosci,
+            bladSredniPolozenia,
             ukladWspolrzednychPoziomych,
             ukladWspolrzednychPionowych,
             calyArkuszWypelnionyTrescia,
@@ -27,6 +28,7 @@ class Nmt:
         self.charakterystykaPrzestrzenna = float(charakterystykaPrzestrzenna.split()[0])
         self.format = format
         self.bladSredniWysokosci = float(bladSredniWysokosci)
+        self.bladSredniPolozenia = float(bladSredniPolozenia)
         self.ukladWspolrzednych = ukladWspolrzednychPoziomych
         self.ukladWysokosci = ukladWspolrzednychPionowych
         self.calyArkuszWyeplnionyTrescia = calyArkuszWypelnionyTrescia
