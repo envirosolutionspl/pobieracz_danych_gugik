5825b9c517f2d40bf14fa73427187e2744eb90eb
aglu <alek.glu@gmail.com>
2024-06-20 16:55:20 +0200

    poprawki - konwertowanie danych wms

--- reflectance_api.py
+++ reflectance_api.py
@@ -38,14 +38,14 @@ def getReflectanceListbyPoint1992(point):
 
 def _convert_attributes(elems_list):
     for elem in elems_list:
-        if hasattr(elem, 'aktualnosc'):
-            elem.aktualnosc = datetime.datetime.strptime(elem.aktualnosc, '%Y-%m-%d').date()
-        if hasattr(elem, 'wielkoscPiksela'):
-            elem.wielkoscPiksela = float(elem.wielkoscPiksela)
-        if hasattr(elem, 'zakresIntensywnosci'):
-            elem.elemzakresIntensywnosci = int(elem.zakresIntensywnosci)
-        if hasattr(elem, 'aktualnoscRok'):
-            elem.aktualnoscRok = int(elem.aktualnoscRok)
-        if hasattr(elem, 'dt_pzgik'):
-            elem.dt_pzgik = str(elem.dt_pzgik)
+        if 'aktualnosc' in elem:
+            elem['aktualnosc'] = datetime.datetime.strptime(elem.get('aktualnosc'), '%Y-%m-%d').date()
+        if 'wielkoscPiksela' in elem:
+            elem['wielkoscPiksela'] = float(elem.get('wielkoscPiksela'))
+        if 'zakresIntensywnosci' in elem:
+            elem['elemzakresIntensywnosci'] = int(elem.get('zakresIntensywnosci'))
+        if 'aktualnoscRok' in elem:
+            elem['aktualnoscRok'] = int(elem.get('aktualnoscRok'))
+        if 'dt_pzgik' in elem:
+            elem['dt_pzgik'] = str(elem.get('dt_pzgik'))
     return elems_list
