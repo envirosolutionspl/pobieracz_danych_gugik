56f12b8187280344ceca4e382999754a8721fcc9
wojtek19022 <113895660+wojtek19022@users.noreply.github.com>
2024-07-13 02:15:52 +0200

    Dokumentacja zmian v1.2.2

--- pobieracz_danych_gugik.py
+++ pobieracz_danych_gugik.py
@@ -30,7 +30,7 @@ from . import utils, ortofoto_api, nmt_api, nmpt_api, service_api, las_api, refl
     mozaika_api, wizualizacja_karto_api, kartoteki_osnow_api, zdjecia_lotnicze_api, egib_api, mesh3d_api
 
 """Wersja wtyczki"""
-plugin_version = '1.2.1'
+plugin_version = '1.2.2'
 plugin_name = 'Pobieracz Danych GUGiK'
 
 
