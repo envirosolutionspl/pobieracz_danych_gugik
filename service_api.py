f9b92484e3aaedc5cc6d7e749825351f9fe56542
agluszek <77581802+agluszek@users.noreply.github.com>
2024-08-06 13:33:58 +0200

    #139 Poprawa działania wtyczki przy przerwaniu połączenia(#159)
    Co-authored-by: aglu <alek.glu@gmail.com>
    Co-authored-by: Wojciech Sołyga <113895660+wojtek19022@users.noreply.github.com>

--- service_api.py
+++ service_api.py
@@ -17,11 +17,9 @@ def getRequest(params, url):
                     return True, resp.text
                 else:
                     return False, f'Błąd {resp.status_code}'
-                
         except requests.exceptions.ConnectionError:
             attempt += 1
             time.sleep(2)
-        
         except requests.exceptions.ReadTimeout:
             attempt += 1
             time.sleep(2)
@@ -29,25 +27,22 @@ def getRequest(params, url):
 
 def retreiveFile(url, destFolder, obj):
     file_name = url.split('/')[-1]
-
     if '?' in file_name:
         file_name = (file_name.split('?')[-1].replace('=', '_')) + '.zip'
-    else:
-        pass
 
     if 'Budynki3D' in url:
         if 'LOD1' in url:
-            file_name = "Budynki_3D_LOD1_" + file_name
+            file_name = f"Budynki_3D_LOD1_{file_name}"
         elif 'LOD2' in url:
-            file_name = "Budynki_3D_LOD2_" + file_name
+            file_name = f"Budynki_3D_LOD2_{file_name}"
 
         if len(url.split('/')) == 9:
             file_name = url.split('/')[6] + '_' + file_name
 
     elif 'PRG' in url:
-        file_name = "PRG_" + file_name
-    elif 'bdot10k' in url and not 'Archiwum' in url:
-        file_name = "bdot10k_" + file_name
+        file_name = f"PRG_{file_name}"
+    elif 'bdot10k' in url and 'Archiwum' not in url:
+        file_name = f"bdot10k_{file_name}"
     elif 'Archiwum' in url and 'bdot10k' in url:
         file_name = "archiwalne_bdot10k_" + url.split('/')[5] + '_' + file_name
     elif 'bdoo' in url:
@@ -55,10 +50,9 @@ def retreiveFile(url, destFolder, obj):
     elif 'ZestawieniaZbiorczeEGiB' in url:
         file_name = "ZestawieniaZbiorczeEGiB_" + 'rok' + url.split('/')[4] + '_' + file_name
     elif 'osnowa' in url:
-        file_name = "podstawowa_osnowa_" + file_name
+        file_name = f"podstawowa_osnowa_{file_name}"
 
     path = os.path.join(destFolder, file_name)
-
     try:
         resp = get_legacy_session().get(url=url, verify=False, stream=True, timeout=10)
         if str(resp.status_code) == '404':
@@ -72,6 +66,8 @@ def retreiveFile(url, destFolder, obj):
                 with open(path, 'wb') as f:
                     for chunk in resp.iter_content(chunk_size=8192):
                         """Pobieramy plik w kawałkach dzięki czemu możliwe jest przerwanie w trakcie pobierania"""
+                        if not check_internet_connection():
+                            return False, 'Połączenie zostało przerwane'
                         if obj.isCanceled():
                             resp.close()
                             saved = False
@@ -84,13 +80,11 @@ def retreiveFile(url, destFolder, obj):
                 return True
             else:
                 os.remove(path)
-                return False, "Pobieranie przerwane"
-            
+                return False, "Pobieranie przerwane" 
     except requests.exceptions.Timeout:
         return False
-    except requests.exceptions.ConnectionError:
-        retreiveFile(url, destFolder)
-        return True
+    except requests.exceptions.ConnectionError as err:
+        return False, err
 
 def getAllLayers(url,service):
     params = {
@@ -119,13 +113,11 @@ def getAllLayers(url,service):
 def check_internet_connection():
     try:
         resp = get_legacy_session().get(url='https://uldk.gugik.gov.pl/', verify=False, timeout=20)
-        if resp.status_code != 200:
-            return False
-        else:
-            return True
+        return resp.status_code == 200
+    except requests.exceptions.Timeout:
+        return False
     except requests.exceptions.ConnectionError:
         return False
-
 
 if __name__ == '__main__':
     url = "https://opendata.geoportal.gov.pl/ortofotomapa/73214/73214_897306_N-34-91-C-d-1-4.tif"
