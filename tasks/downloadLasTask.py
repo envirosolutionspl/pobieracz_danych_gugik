efacc1dc07e26de835a2d25ce28063afaaad6bc7
wojtek19022 <113895660+wojtek19022@users.noreply.github.com>
2024-10-09 18:05:25 +0200

    Ujednolicenie pobierania danych las

--- tasks/downloadLasTask.py
+++ tasks/downloadLasTask.py
@@ -54,7 +54,7 @@ class DownloadLasTask(QgsTask):
         to do GUI operations and raise Python exceptions here.
         result is the return value from self.run.
         """
-        if result:
+        if result and self.exception != 'Połączenie zostało przerwane':
             QgsMessageLog.logMessage('sukces')
             self.iface.messageBar().pushMessage(
                 'Sukces',
@@ -67,7 +67,6 @@ class DownloadLasTask(QgsTask):
                 QgsMessageLog.logMessage('finished with false')
             elif isinstance(self.exception, BaseException):
                 QgsMessageLog.logMessage("exception")
-                raise self.exception
             self.iface.messageBar().pushWarning(
                 'Błąd',
                 'Dane LAZ nie zostały pobrane.'
