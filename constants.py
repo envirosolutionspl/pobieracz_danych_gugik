f77a3826afc36e44506721c35f3cf0266e333cc7
aglu <alek.glu@gmail.com>
2024-11-04 14:48:15 +0100

    qgis feed update

--- constants.py
+++ constants.py
@@ -247,3 +247,20 @@ POZIOMY_UPROSZCZENIA = {
     50000000: 100000
 
 }
+
+
+INDUSTRIES = {
+    "999": "Nie wybrano",
+    "e": "Energetyka/OZE",
+    "u": "Urząd",
+    "td": "Transport/Drogi",
+    "pg": "Planowanie/Geodezja",
+    "wk": "WodKan",
+    "s": "Środowisko",
+    "rl": "Rolnictwo/Leśnictwo",
+    "tk": "Telkom",
+    "edu": "Edukacja",
+    "i": "Inne",
+    "it": "IT",
+    "n": "Nieruchomości"
+}
--- qgis_feed.py
+++ qgis_feed.py
@@ -1,33 +1,142 @@
-from qgis.core import QgsNewsFeedParser, QgsSettings, QgsNewsFeedModel, QgsMessageLog
+from qgis.core import (
+    QgsNewsFeedParser,
+    QgsSettings,
+    QgsMessageLog
+)
 from qgis.PyQt.QtCore import QUrl
+from qgis.PyQt import uic
+from qgis.PyQt.QtWidgets import QDialog, QComboBox, QPushButton
+
 import re
+import os
+import unicodedata
+
+from .constants import INDUSTRIES
+
 
 class QgisFeed:
-    def __init__(self):
+    def __init__(self, selected_industry, plugin_name):
         self.s = QgsSettings()
-        self.envirosolutionsFeedPattern_old = re.compile(r'core/NewsFeed/httpsqgisfeedenvirosolutionspl')
-        self.envirosolutionsFeedPattern_new = re.compile(r'app/news-feed/items/httpsqgisfeedenvirosolutionspl/entries/items')
-        # self.envirosolutionsFeedPattern = re.compile('core/NewsFeed/httpsqgisfeedenvirosolutionspl/\d+/\w+')
-        self.parser = QgsNewsFeedParser(QUrl("https://qgisfeed.envirosolutions.pl/"))
+        self.industries_dict = INDUSTRIES
+
+        self.industry_decoded = [key for key, val in self.industries_dict.items() if val == selected_industry]
+        self.plugin_name_slug = self.create_slug(plugin_name)
+
+        self.es_url = (
+            "https://qgisfeed.envirosolutions.pl/"
+            if not self.industry_decoded
+            else f"https://qgisfeed.envirosolutions.pl/?industry={self.industry_decoded[0]}&plugin={self.plugin_name_slug}"
+        )
+        self.parser = QgsNewsFeedParser(
+            feedUrl=QUrl(self.es_url)
+        )
+        self.industry_url_short = self.shortenUrl(self.es_url)
+        self.envirosolutionsFeedPattern_old = re.compile(f"core/NewsFeed/{self.industry_url_short}")
+        self.envirosolutionsFeedPattern_new = re.compile(f"app/news-feed/items/{self.industry_url_short}")
+
         self.parser.fetched.connect(self.registerFeed)
 
+    def shortenUrl(self, url):
+        """
+        Funkcja przetwarza zapisany adres qgisfeed'a 
+        na forme zapisana w qgis settingsach
+        """
+        return re.sub(r'://|\.|:|/\?|=|&|-', '', url)
+
+    def create_slug(self, text):
+        """
+        This function makes slug from a random text
+        """
+        slug = self.normalizeString(text)
+        slug = re.sub(r'[^a-z0-9\s-]', '', slug.lower())  # Remove non-alphanumeric characters except spaces and hyphens
+        slug = re.sub(r'[\s]+', '-', slug)  # Replace spaces with hyphens
+
+        return slug.strip('-')
+
+    def normalizeString(self, text):
+        return ''.join(part for part in unicodedata.normalize('NFD', text)
+                       if unicodedata.category(part) != 'Mn')
+
     def registerFeed(self):
+        """
+        Function registers QGIS Feed
+        """
+        print("New url: ", self.industry_url_short)
         QgsMessageLog.logMessage('Registering feed')
         for key in self.s.allKeys():
             if self.envirosolutionsFeedPattern_old.match(key) or self.envirosolutionsFeedPattern_new.match(key):
-                finalKey = re.sub(r'(\d+)', r'9999\1', key.replace('httpsqgisfeedenvirosolutionspl', 'httpsfeedqgisorg'))
+                finalKey = re.sub(
+                    r'(\d+)',
+                    r'9999\1',
+                    key.replace(self.industry_url_short, 'httpsfeedqgisorg')
+                )
                 self.s.setValue(finalKey, self.s.value(key))
 
+            # ponizszy fragment odpowiada za mozliwosc ciaglego wyswietlania wiadomosci
+            # przy wlaczeniu qgis za kazdym razem
+
+            check_fetch = self.checkIsFetchTime()
+            if check_fetch is True: self.s.remove(key)
+
     def removeDismissed(self):
+        """
+        Function checks whether there was already initialized QGIS Feed
+        """
+
         for key in self.s.allKeys():
             if self.envirosolutionsFeedPattern_old.match(key) or self.envirosolutionsFeedPattern_new.match(key):
                 # sprawdz czy jest odpowiadajacy w qgis
-                if not self.s.contains(re.sub(r'(\d+)', r'9999\1', key.replace('httpsqgisfeedenvirosolutionspl', 'httpsfeedqgisorg'))):
+                if self.s.contains(
+                        re.sub(
+                            r'(\d+)',
+                            r'9999\1',
+                            key.replace(self.industry_url_short, 'httpsfeedqgisorg')
+                        )
+                ):
                     self.s.remove(key)
+                self.s.remove(key)
+
+    def checkIsFetchTime(self):
+        """
+        Function check if the fetch time from QGIS Feed was already registered
+        """
+        return self.s.contains(f"core/NewsFeed/{self.industry_url_short}/lastFetchTime") \
+            or self.s.contains(f"app/news-feed/items/{self.industry_url_short}/last-fetch-time")
 
     def initFeed(self):
-        if self.s.contains('core/NewsFeed/httpsqgisfeedenvirosolutionspl/lastFetchTime') \
-                or self.s.contains('app/news-feed/items/httpsqgisfeedenvirosolutionspl/last-fetch-time'):  # już istnieje feed
-            self.removeDismissed()
+        """
+        Function is a built in QGIS class and it is responsible for firing QGIS Feed
+        """
+
+        check_fetch = self.checkIsFetchTime()
+        if check_fetch is True: self.removeDismissed()
         self.parser.fetch()
 
+
+class QgisFeedDialog(QDialog):
+    def __init__(self, parent=None):
+        super(QgisFeedDialog, self).__init__(parent)
+        self.ui_file_path = os.path.join(os.path.dirname(__file__), 'ui', 'qgis_feed.ui')
+        uic.loadUi(self.ui_file_path, self)
+
+        self.comboBox = self.findChild(QComboBox, 'comboBox')
+        self.pushButton = self.findChild(QPushButton, 'pushButton')
+        self.pushButton.clicked.connect(self.onSaveClicked)
+
+        self.loadPreviousSelection()
+
+    def loadPreviousSelection(self):
+        settings = QgsSettings()
+        previous_selection = settings.value("selected_industry")
+        if previous_selection:
+            index = self.comboBox.findText(previous_selection)
+            if index != -1:
+                self.comboBox.setCurrentIndex(index)
+            self.hide()
+
+    def onSaveClicked(self):
+        # zapisz wybraną branżę
+        selected_industry = self.comboBox.currentText()
+        settings = QgsSettings()
+        settings.setValue("selected_industry", selected_industry)
+        self.accept()
