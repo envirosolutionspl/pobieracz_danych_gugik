bdce93977432a8e3d097d926ba1942431102044c
darek-r <117411844+darek-r@users.noreply.github.com>
2026-04-27 16:51:27 +0200

    Poprawiona obsługa plików xml ( #264 )

--- wfs/wfs_egib.py
+++ wfs/wfs_egib.py
@@ -1,5 +1,5 @@
 import os
-from ..defusedxml import ElementTree as ET
+from ..libs.defusedxml import ElementTree as ET
 from time import sleep
 from lxml import etree
 from lxml.etree import XMLSyntaxError
