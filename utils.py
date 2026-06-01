c151076c91caff88a4de4bf328716bdd3cca8618
milenart <mikolaj.lenart17@gmail.com>
2026-06-01 11:11:05 +0200

    Obsluga importu urllib3

--- utils.py
+++ utils.py
@@ -49,10 +49,17 @@ from .constants import (
 )
 from functools import partial
 import lxml.etree as ET
+
 try:
-    from urllib3.util.ssl_ import create_urllib3_context
+    if urllib3.__version__.startswith('1.'):
+        from urllib3.util.ssl_ import create_urllib3_context
+    else:
+        from urllib3.util import create_urllib3_context
 except ImportError:
-    from urllib3.util import create_urllib3_context
+    QMessageBox.critical(None, "Błąd biblioteki", 
+                         "Wystąpił problem z wersją urllib3. Wtyczka może nie działać poprawnie.")
+    create_urllib3_context = None
+    
 
 class LegacySslAdapter(requests.adapters.HTTPAdapter):
     """Adapter dopuszczający stare połączenia SSL"""
